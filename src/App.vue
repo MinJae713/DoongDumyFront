f0576b27883448044d171d9bcf47df60a21f73ee
dbalsend@naver.com <dbalsend@naver.com>
2026-04-13 16:37:30 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import DumyPage from './pages/DumyPage.vue'
+</script>
+
+<template>
+  <DumyPage />
+</template>
+
+<style scoped></style>
---
+++ src/pages/DumyPage.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <h1>배포 연습중입니다</h1>
+</template>
+
+<style scoped></style>
